8ffac1b76777d6e42740ce624a85319bb9d4218b
testingmyluck <147545715+testingmyluck@users.noreply.github.com>
2024-01-14 15:51:46 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -18,17 +18,11 @@ CORS(app, resources={
 })
 
 def extract_post_id(api_key, blog_id, post_url):
-    # Extract post path from the post URL
     post_path = post_url.replace('https://www.hotnippy.com', '')
-
-    # Construct the API request URL
     url = f'https://www.googleapis.com/blogger/v3/blogs/{blog_id}/posts/bypath?path={post_path}&key={api_key}'
 
     try:
-        # Send a GET request to retrieve the post
         response = requests.get(url)
-
-        # Check if the request was successful (status code 200)
         if response.status_code == 200:
             post_data = response.json()
             post_id = post_data.get('id')
@@ -39,20 +33,40 @@ def extract_post_id(api_key, blog_id, post_url):
         print(f"An error occurred: {e}")
         return None
 
-@app.route('/get_post_id', methods=['POST'])
-def get_post_id():
+def extract_post_labels(api_key, blog_id, post_url):
+    post_path = post_url.replace('https://www.hotnippy.com', '')
+    url = f'https://www.googleapis.com/blogger/v3/blogs/{blog_id}/posts/bypath?path={post_path}&key={api_key}'
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            post_data = response.json()
+            post_labels = post_data.get('labels', [])
+            return post_labels
+        else:
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+@app.route('/get_post_data', methods=['POST'])
+def get_post_data():
     api_key = 'AIzaSyBoE_mDhkMHdUgwvpoK3RcKbAPAsOu9Muk'
     blog_id = '6331170148874248090'
-
-    # Get post_url from the request JSON data
+    
     post_url = request.json.get('post_url')
 
     post_id = extract_post_id(api_key, blog_id, post_url)
+    post_labels = extract_post_labels(api_key, blog_id, post_url)
 
-    if post_id:
-        return jsonify({'post_id': post_id})
+    if post_id is not None:
+        response_data = {
+            'post_id': post_id,
+            'post_labels': post_labels
+        }
+        return jsonify(response_data)
     else:
-        return jsonify({'error': 'Failed to retrieve post ID'}), 500
+        return jsonify({'error': 'Failed to retrieve post data'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
