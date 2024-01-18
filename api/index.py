b5e5e9b23988ccf90a573d89ae21b71e9374bdae
testingmyluck <147545715+testingmyluck@users.noreply.github.com>
2024-01-18 12:46:30 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 # Get the port number from the PORT environment variable, or use a default value (5000)
-port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 5001))
 
 # Configure CORS to allow requests from multiple websites
 CORS(app, resources={
@@ -18,7 +18,7 @@ CORS(app, resources={
 })
 
 
-def extract_post_id(api_key, blog_id, post_url):
+def extract_post_data(api_key, blog_id, post_url):
     post_path = post_url.replace('https://www.hotnippy.com', '')
     url = f'https://www.googleapis.com/blogger/v3/blogs/{blog_id}/posts/bypath?path={post_path}&key={api_key}'
 
@@ -27,38 +27,22 @@ def extract_post_id(api_key, blog_id, post_url):
         if response.status_code == 200:
             post_data = response.json()
             post_id = post_data.get('id')
-            return post_id
-        else:
-            return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
-
-def extract_post_labels(api_key, blog_id, post_url):
-    post_path = post_url.replace('https://www.hotnippy.com', '')
-    url = f'https://www.googleapis.com/blogger/v3/blogs/{blog_id}/posts/bypath?path={post_path}&key={api_key}'
-
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            post_data = response.json()
             post_labels = post_data.get('labels', [])
-            return post_labels
+            return post_id, post_labels
         else:
-            return None
+            return None, None
     except Exception as e:
         print(f"An error occurred: {e}")
-        return None
+        return None, None
 
-@app.route('/get_post_data', methods=['POST'])
+@app.route('/get_post_data', methods=['GET'])
 def get_post_data():
     api_key = 'AIzaSyBoE_mDhkMHdUgwvpoK3RcKbAPAsOu9Muk'
     blog_id = '6331170148874248090'
     
-    post_url = request.json.get('post_url')
+    post_url = request.args.get('post_url')
 
-    post_id = extract_post_id(api_key, blog_id, post_url)
-    post_labels = extract_post_labels(api_key, blog_id, post_url)
+    post_id, post_labels = extract_post_data(api_key, blog_id, post_url)
 
     if post_id is not None:
         response_data = {
@@ -70,4 +54,4 @@ def get_post_data():
         return jsonify({'error': 'Failed to retrieve post data'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=port)
