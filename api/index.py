6b92b73da99eb9df9b153cfcf04f2d895c95ef9e
testingmyluck <147545715+testingmyluck@users.noreply.github.com>
2024-01-18 17:02:28 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS 
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
 def extract_post_labels(api_key, blog_id, post_url):
     post_path = post_url.replace('https://www.hotnippy.com', '')
@@ -30,8 +30,8 @@ def get_labels():
     labels = extract_post_labels(api_key, blog_id, post_url)
 
     if labels is not None:
-        response = jsonify({'labels': labels})
-        response.headers.add('Access-Control-Allow-Origin', '*')  # Allow requests from any origin
+        response = jsonify({'post_labels': labels})
+        response.headers.add('Access-Control-Allow-Origin', '*')
         return response
     else:
         return jsonify({'error': 'Failed to retrieve labels'}), 500
